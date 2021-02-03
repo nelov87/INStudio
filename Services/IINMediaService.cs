e595809c3ddcdec95bd71678f1d22a565081c069
nelov87 <nelov87@gmail.com>
2021-02-03 23:23:36 +0200

    Test

--- Services/IINMediaService.cs
+++ Services/IINMediaService.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System;
+using INStudio.Data;
+
 
 namespace INStudio.Services
 {
     public interface IINMediaService
     {
-                
+        ICollection<INMedia> GetAll();
     }
 }
--- Services/INMediaService.cs
+++ Services/INMediaService.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using INStudio.Data;
+
 namespace INStudio.Services
 {
     public class INMediaService : IINMediaService
     {
-        
+        public ICollection<INMedia> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
